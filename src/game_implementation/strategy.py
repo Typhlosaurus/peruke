f9072a724cbcc533a683881fddd87e4c6a74ff0e
Mark Oppenheim <markopp@gmail.com>
2021-06-10 19:00:54 +0200

    Add double-take strategy

--- src/game_implementation/strategy.py
+++ src/game_implementation/strategy.py
@@ -1,6 +1,6 @@
 import random
 from abc import ABC
-from typing import Any, Collection, Dict
+from typing import Any, Collection, Dict, Tuple
 
 from game_implementation.action import Action
 from game_implementation.disc_state import DiscState
@@ -39,3 +39,26 @@ class TallestDaisyStrategy(SimpleSortedStrategy):
             self.action_preference[action.new_state],
             -game.players[action.target_id].expected_score,
         )
+
+
+class PreferTakeOnDoubleSafeDie(Strategy):
+    def __init__(self, action_preference: Dict[Tuple[DiscState, bool], int]):
+        self.action_preference = action_preference
+
+    def ordering(self, game: Game, action: Action, same_die_available: bool):
+        # prefer first by action type and extra copies of dice available, then by highest score
+        return (
+            self.action_preference[(action.new_state, same_die_available)],
+            -game.players[action.target_id].expected_score,
+        )
+
+    def choose_actions(self, game: Game, player_id: PlayerId, dice: Collection[DiscId]) -> Collection[Action]:
+        remaining_dice = list(dice)
+        while len(remaining_dice) > 0:
+            die = remaining_dice.pop()
+            possible_actions = game.possible_actions(player_id, die)
+            if len(possible_actions) > 0:
+                ordered = sorted(
+                    possible_actions, key=lambda action: self.ordering(game, action, die in remaining_dice)
+                )
+                yield ordered[-1]
--- src/test_harness.py
+++ src/test_harness.py
@@ -2,16 +2,27 @@ import random
 
 from game_implementation.disc_state import DiscState
 from game_implementation.game_runner import run_games
-from game_implementation.strategy import RandomStrategy, TallestDaisyStrategy
+from game_implementation.strategy import PreferTakeOnDoubleSafeDie, RandomStrategy, TallestDaisyStrategy
+
+
+random_strategy = RandomStrategy()
+tallest_daisy = TallestDaisyStrategy({DiscState.Gone: 3, DiscState.Safe: 2, DiscState.Vulnerable: 2})
+prefer_double_take = PreferTakeOnDoubleSafeDie(
+    {
+        (DiscState.Gone, True): 3,
+        (DiscState.Gone, False): 3,
+        (DiscState.Safe, True): 1,
+        (DiscState.Safe, False): 1,
+        (DiscState.Vulnerable, True): 2,
+        (DiscState.Vulnerable, False): 0,
+    }
+)
 
 
 def make_strategies():
-    test_strategy = TallestDaisyStrategy({DiscState.Gone: 3, DiscState.Safe: 2, DiscState.Vulnerable: 2})
-    random_strategy = RandomStrategy()
-
     return [
-        random_strategy,
-        test_strategy,
+        tallest_daisy,
+        prefer_double_take,
         random_strategy,
     ]
 
